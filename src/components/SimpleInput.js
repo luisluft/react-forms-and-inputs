f49176b149adb32fc09f288954d402d272e83407
luisluft <luberluft@gmail.com>
2021-11-12 11:20:12 -0400

    add validation to the name input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,41 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() == "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+  };
+
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
-    <form>
-      <div className="form-control">
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          onChange={nameInputChangeHandler}
+          type="text"
+          id="name"
+          value={enteredName}
+        />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name can't be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
